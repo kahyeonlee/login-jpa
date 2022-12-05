e3d2c24dd2a8577845d1b15d0e7637efaa12124b
gahyun <gmeoqq@gmail.com>
2022-12-05 11:21:32 +0900

    feat : Add Service-login기능
    username이 없을때, password 틀릴때 error코드 지정
    Exception 없을때 토큰 발행

--- src/main/java/com/example/loginjpa/exception/ErrorCode.java
+++ src/main/java/com/example/loginjpa/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    USERNAME_DUPLICATED(HttpStatus.CONFLICT, "");
+    USERNAME_DUPLICATED(HttpStatus.CONFLICT, ""),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "");
 
     private HttpStatus httpStatus;
 
--- src/main/java/com/example/loginjpa/service/UserService.java
+++ src/main/java/com/example/loginjpa/service/UserService.java
@@ -4,9 +4,13 @@ import com.example.loginjpa.domain.User;
 import com.example.loginjpa.exception.AppException;
 import com.example.loginjpa.exception.ErrorCode;
 import com.example.loginjpa.repository.UserRepository;
+import com.example.loginjpa.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +18,10 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.token.secret}")
+    private String key;
+    private Long expireTimeMs= 1000*60*60l;
 
     public String join(String userName,String password){
 
@@ -31,5 +39,19 @@ public class UserService {
         userRepository.save(user);
 
         return "SUCCESS";
+    }
+
+    public String login(String userName, String password) {
+        //username 없음
+        User selectedUser = userRepository.findByUserName(userName)
+                .orElseThrow(()->new AppException(ErrorCode.USERNAME_NOT_FOUND,userName+"이 없습니다."));
+
+        //password 틀림
+        if(!encoder.matches(password,selectedUser.getPassword())){
+            throw new AppException(ErrorCode.INVALID_PASSWORD, "패스워드를 잘못 입력하셨습니다.");
+        }
+        //앞에서 Exception안나면 토큰 발행
+        String token = JwtTokenUtil.createToken(selectedUser.getUserName(), key, expireTimeMs);
+        return token;
     }
 }
